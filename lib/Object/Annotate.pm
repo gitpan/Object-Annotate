822fca6a9d4aed78a0384afb2667d37ed4a48028
Ricardo SIGNES <rjbs@cpan.org>
2013-07-07 12:34:13 +0000

    Import of RJBS/Object-Annotate-0.022 from CPAN.
    gitpan-cpan-distribution: Object-Annotate
    gitpan-cpan-version:      0.022
    gitpan-cpan-path:         RJBS/Object-Annotate-0.022.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Object/Annotate.pm
+++ lib/Object/Annotate.pm
@@ -1,72 +1,14 @@
-
-package Object::Annotate;
-use strict;
 use warnings;
-
-=head1 NAME
-
-Object::Annotate - mix in logging-to-database to objects
-
-=head1 VERSION
-
- $Id$
-
-version 0.021
-
-=cut
-
-our $VERSION = '0.021';
+use strict;
+package Object::Annotate;
+{
+  $Object::Annotate::VERSION = '0.022';
+}
+# ABSTRACT: mix in logging-to-database to objects (deprecated)
 
 use Carp ();
-use UNIVERSAL::moniker;
+use UNIVERSAL::moniker 0.01;
 
-=head1 SYNOPSIS
-
-  package Your::Class;
-  use Object::Annotate annotate => { dsn => '...', table => 'notes' };
-
-  ...
-
-  my $object = Your::Class->new( ... );
-  $object->annotate({ event => "created", comment => "(as example)" });
-
-=head1 WARNING
-
-The interface described here is still in real flux.  Feedback is welcome!
-If, however, you rely on this interface for production code, and then upgrade
-this module blindly, you are likely to see things break.
-
-I will remove this warning when the interface is less likely to change.
-
-=head1 DESCRIPTION
-
-Object::Annotate is a mixin that provides any class with method for storing
-and retrieving notes about its objects.  It can also produce objects which
-exist only to store annotations about abstract (uninstantiated) objects,
-procedures, or concepts.
-
-=head1 USAGE
-
-To mix Object::Annotate into a class, just C<use> it.  To create a classless
-annotator object, use Object::Annotate's C<new> method.  Both of these usages
-accept the same arguments:
-
-  db        - options for the database in which notes are stored; a hashref:
-
-    dsn       - the DSN to pass to Class::DBI to create a connection
-    user      - the username to use in connecting to the database
-    pass      - the password to use in connecting to the database
-    table     - the table in which annotations are stored
-    sequence  - if given, the Class::DBI table's primary key values comes from
-                this sequence; see L<Class::DBI> for more information
-
-  columns   - columns for the annotation table
-  obj_class - the class name to use for annotations for this class
-              (defaults to Class->moniker, see UNIVERSAL::moniker)
-  id_attr   - the object attribute to use for "id"; called as a method
-              if it's a scalar ref, it's de-ref'd and used as a constant string
-
-=cut
 
 # We'll store the constructed Class::DBI subclasses here.
 # $class_for->{ $dsn }->{ $table } = $class
@@ -83,19 +25,10 @@ my %note_columns = (
   default   => [ qw(event attr old_val new_val via comment expire_time) ],
 );
 
-use Sub::Exporter -setup => {
+use Sub::Exporter 0.92 -setup => {
   groups => { annotator => \&setup_class },
 };
 
-=head2 new
-
-You can use the C<new> method to create a singularity -- an object that can
-annotate as if it was of a class that used Object::Annotate, but is of its own
-unique class.
-
-  my $notepad = Object::Annotate->new({ db => { ... } });
-
-=cut
 
 sub new {
   my ($self, $arg) = @_;
@@ -110,53 +43,7 @@ sub new {
   bless $singularity => $target;
 }
 
-=head1 METHODS
 
-These methods are not provided by Object::Annotate, but are installed into
-classes that use Object::Annotate.
-
-=head2 annotations_class
-
-  my $annotations_class = Your::Class->annotations_class;
-
-This method returns the name of the automatically constructed class that
-handles annotations for the class or object on which it is installed.
-
-=head2 annotate
-
-  $object->annotate({
-    event => 'update',
-    attr  => 'priority',
-    old_val => 1,
-    new_val => 3,
-  });
-
-This method creates an annotation for the object on which it is called.
-
-=head2 search_annotations
-
-  # search all annotations for this class
-  my @notes = Class->search_annotations({ event => 'explosion' });
-
-  # searches only annotations for this object
-  my @notes = $object->search_annotations({ event => 'explosion' });
-
-This method searches through the annotations for a class or an object, using
-the Class::DBI C<search> method.
-
-=cut
-
-=head1 INTERNALS
-
-=head2 setup_class
-
-  Object::Annotate->setup_class('annotator', \%arg, \%col);
-
-This method does the heavy lifting needed to turn the class named by C<$target>
-into one that does annotation.  It is a group generator as described in
-L<Sub::Exporter>.
-
-=cut
 
 sub setup_class {
   my ($self, $name, $arg, $col) = @_;
@@ -178,7 +65,7 @@ sub setup_class {
   my $class     = $self->class_for($arg);
 
   my $obj_class = $arg->{obj_class};
-  
+
   my %build_option = (
     obj_class => $obj_class,
     id_attr   => $arg->{id_attr} || 'id',
@@ -200,20 +87,6 @@ sub setup_class {
   };
 }
 
-=head2 class_for
-
-  my $class = Object::Annotate->class_for(\%arg);
-
-This method returns the class to use for the described database and table,
-constructing it (see C<L</construct_class>>) if needed.
-
-Valid arguments are (for all, see the L</USAGE> section): dsn, table, db_user,
-db_pass, sequence
-
-See the L</USAGE> section, above, for information on these arguments, which
-typically are passed along by the import routine.
-
-=cut
 
 sub class_for {
   my ($self, $arg) = @_;
@@ -248,24 +121,6 @@ sub class_for {
   return $class;
 }
 
-=head2 default_dsn
-
-=head2 default_table
-
-=head2 default_user
-
-=head2 default_pass
-
-These methods return the default database settings to use if none is specified
-when importing Object::Annotate.  The built-in behavior is to return the
-OBJ_ANNOTATE_DSN, OBJ_ANNOTATE_TABLE, etc. environment variables.
-
-=head2 default_base_class
-
-This method returns the class from which the annotator subclass will inherit.
-It defaults to Class::DBI.
-
-=cut
 
 sub default_dsn   { $ENV{OBJ_ANNOTATE_DSN};   }
 sub default_table { $ENV{OBJ_ANNOTATE_TABLE}; }
@@ -275,23 +130,6 @@ sub default_base_class  { 'Class::DBI' }
 
 sub _default_sequence {  }
 
-=head2 construct_cdbi_class
-
-  my $new_class = Object::Annotate->construct_cdbi_class(\%arg);
-
-This method sets up a new Class::DBI subclass that will store in the database
-described by the arguments.
-
-Valid arguments are:
-
-  dsn     - the dsn for the database in which to store
-  user    - the database user as whom to connect
-  pass    - the database password
-  table   - the table in which to store annotations
-  columns - the extra columns for the table
-  base_class - class from which the new class inherits (default: Class::DBI)
-
-=cut
 
 sub construct_cdbi_class {
   my ($class, $arg) = @_;
@@ -326,21 +164,6 @@ sub construct_cdbi_class {
   return $class_for->{ $arg->{dsn} || '' }->{ $arg->{table} } = $new_class;
 }
 
-=head2 build_annotator
-
-  my $code = Object::Annotate->build_annotator(\%arg);
-
-This builds the routine that will be installed as "annotate" in the importing
-class.  It returns a coderef.
-
-It takes the following arguments:
-
-  obj_class - the class name to use for this class's log entries
-  id_attr   - the method to use to get object ids; if a scalar ref, 
-              the dereferenced string is used as a constant
-  set_time  - if true, the created value will be created as the current time
-
-=cut
 
 sub build_annotator {
   my ($self, $arg) = @_;
@@ -349,7 +172,7 @@ sub build_annotator {
   my $id_attr   = $arg->{id_attr};
   my $set_time  = $arg->{set_time};
 
-  my @columns 
+  my @columns
     = $arg->{columns} ? @{ $arg->{columns} } : @{ $note_columns{default} };
 
   my $noun      = $arg->{noun};
@@ -390,20 +213,6 @@ sub build_annotator {
   return $annotator;
 }
 
-=head2 build_searcher
-
-  my $code = Object::Annotate->build_searcher(\%arg);
-
-This builds the routine that will be installed as "search_annotations" in the
-importing class.  It returns a coderef.
-
-It takes the following arguments:
-
-  obj_class - the class name to use for this class's log entries
-  id_attr   - the method to use to get object ids; if a scalar ref, 
-              the dereferenced string is used as a constant
-
-=cut
 
 sub build_searcher {
   my ($self, $arg) = @_;
@@ -412,7 +221,7 @@ sub build_searcher {
   my $id_attr   = $arg->{id_attr};
 
   my $noun      = $arg->{noun};
-  
+
   my $searcher = sub {
     my ($self, $arg) = @_;
     my $obj_class = $arg->{obj_class} || $self->moniker;
@@ -436,3 +245,193 @@ sub build_searcher {
 
 '2. see footnote #1';
 
+__END__
+
+=pod
+
+=head1 NAME
+
+Object::Annotate - mix in logging-to-database to objects (deprecated)
+
+=head1 VERSION
+
+version 0.022
+
+=head1 SYNOPSIS
+
+B<Achtung!>  This library was an experiment.  It failed.  Consider using
+L<Mixin::ExtraFields> instead.
+
+  package Your::Class;
+  use Object::Annotate annotate => { dsn => '...', table => 'notes' };
+
+  ...
+
+  my $object = Your::Class->new( ... );
+  $object->annotate({ event => "created", comment => "(as example)" });
+
+=head1 DESCRIPTION
+
+Object::Annotate is a mixin that provides any class with method for storing
+and retrieving notes about its objects.  It can also produce objects which
+exist only to store annotations about abstract (uninstantiated) objects,
+procedures, or concepts.
+
+=head1 USAGE
+
+To mix Object::Annotate into a class, just C<use> it.  To create a classless
+annotator object, use Object::Annotate's C<new> method.  Both of these usages
+accept the same arguments:
+
+  db        - options for the database in which notes are stored; a hashref:
+
+    dsn       - the DSN to pass to Class::DBI to create a connection
+    user      - the username to use in connecting to the database
+    pass      - the password to use in connecting to the database
+    table     - the table in which annotations are stored
+    sequence  - if given, the Class::DBI table's primary key values comes from
+                this sequence; see L<Class::DBI> for more information
+
+  columns   - columns for the annotation table
+  obj_class - the class name to use for annotations for this class
+              (defaults to Class->moniker, see UNIVERSAL::moniker)
+  id_attr   - the object attribute to use for "id"; called as a method
+              if it's a scalar ref, it's de-ref'd and used as a constant string
+
+=head2 new
+
+You can use the C<new> method to create a singularity -- an object that can
+annotate as if it was of a class that used Object::Annotate, but is of its own
+unique class.
+
+  my $notepad = Object::Annotate->new({ db => { ... } });
+
+=head1 METHODS
+
+These methods are not provided by Object::Annotate, but are installed into
+classes that use Object::Annotate.
+
+=head2 annotations_class
+
+  my $annotations_class = Your::Class->annotations_class;
+
+This method returns the name of the automatically constructed class that
+handles annotations for the class or object on which it is installed.
+
+=head2 annotate
+
+  $object->annotate({
+    event => 'update',
+    attr  => 'priority',
+    old_val => 1,
+    new_val => 3,
+  });
+
+This method creates an annotation for the object on which it is called.
+
+=head2 search_annotations
+
+  # search all annotations for this class
+  my @notes = Class->search_annotations({ event => 'explosion' });
+
+  # searches only annotations for this object
+  my @notes = $object->search_annotations({ event => 'explosion' });
+
+This method searches through the annotations for a class or an object, using
+the Class::DBI C<search> method.
+
+=head1 INTERNALS
+
+=head2 setup_class
+
+  Object::Annotate->setup_class('annotator', \%arg, \%col);
+
+This method does the heavy lifting needed to turn the class named by C<$target>
+into one that does annotation.  It is a group generator as described in
+L<Sub::Exporter>.
+
+=head2 class_for
+
+  my $class = Object::Annotate->class_for(\%arg);
+
+This method returns the class to use for the described database and table,
+constructing it (see C<L</construct_class>>) if needed.
+
+Valid arguments are (for all, see the L</USAGE> section): dsn, table, db_user,
+db_pass, sequence
+
+See the L</USAGE> section, above, for information on these arguments, which
+typically are passed along by the import routine.
+
+=head2 default_dsn
+
+=head2 default_table
+
+=head2 default_user
+
+=head2 default_pass
+
+These methods return the default database settings to use if none is specified
+when importing Object::Annotate.  The built-in behavior is to return the
+OBJ_ANNOTATE_DSN, OBJ_ANNOTATE_TABLE, etc. environment variables.
+
+=head2 default_base_class
+
+This method returns the class from which the annotator subclass will inherit.
+It defaults to Class::DBI.
+
+=head2 construct_cdbi_class
+
+  my $new_class = Object::Annotate->construct_cdbi_class(\%arg);
+
+This method sets up a new Class::DBI subclass that will store in the database
+described by the arguments.
+
+Valid arguments are:
+
+  dsn     - the dsn for the database in which to store
+  user    - the database user as whom to connect
+  pass    - the database password
+  table   - the table in which to store annotations
+  columns - the extra columns for the table
+  base_class - class from which the new class inherits (default: Class::DBI)
+
+=head2 build_annotator
+
+  my $code = Object::Annotate->build_annotator(\%arg);
+
+This builds the routine that will be installed as "annotate" in the importing
+class.  It returns a coderef.
+
+It takes the following arguments:
+
+  obj_class - the class name to use for this class's log entries
+  id_attr   - the method to use to get object ids; if a scalar ref,
+              the dereferenced string is used as a constant
+  set_time  - if true, the created value will be created as the current time
+
+=head2 build_searcher
+
+  my $code = Object::Annotate->build_searcher(\%arg);
+
+This builds the routine that will be installed as "search_annotations" in the
+importing class.  It returns a coderef.
+
+It takes the following arguments:
+
+  obj_class - the class name to use for this class's log entries
+  id_attr   - the method to use to get object ids; if a scalar ref,
+              the dereferenced string is used as a constant
+
+=head1 AUTHOR
+
+Ricardo SIGNES <rjbs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2006 by Ricardo SIGNES.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
